953abb201bbec16fd00603b794cc672f72a1143e
OdionOseiwe <oseiweifebhor@gmail.com>
2022-11-03 05:00:18 +0100

    updated

---
+++ test/GoveranceAlpha.ts
@@ -0,0 +1,32 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Governor", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function Governor() {
+
+
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount, add3, add4, add5, add6, add7] = await ethers.getSigners();
+
+    const GovernorToken = await ethers.getContractFactory("GovernorAlpha");
+    const governorToken = await GovernorToken.deploy(owner.address);
+
+    return { governorToken, owner, otherAccount, add3, add4, add5, add6, add7 };
+  }
+
+  describe("DELEGATES", function () {
+    it("Checks Balance of Owner", async function () {
+      const { governorToken, owner} = await loadFixture(Governor);
+      const totalsupply = ethers.utils.parseUnits("10000000", "18");
+      expect (await governorToken.balanceOf(owner.address)).to.equal(totalsupply); 
+    });
+
+  });
+
+  
+});
