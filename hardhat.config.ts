e99d6f9c27120ff5ec4624b8b05dbfa4ec595842
OdionOseiwe <oseiweifebhor@gmail.com>
2022-11-03 04:29:27 +0100

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/0h4KWpjRd3xXNW50By-hTTaZDJnNpjiw",
+      }
+    }
+  }
+};
+
+export default config;
